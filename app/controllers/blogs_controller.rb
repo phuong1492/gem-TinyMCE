f58fb69871c110d8ebf6c243fa6d6d63bded5929
phuonghv <phuong@temona.co.jp>
2016-03-23 14:36:24 +0900

    install metric fu

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,25 +1,25 @@
 class BlogsController < ApplicationController
-	def show
-		@blog = Blog.find params[:id]
-	end
-
-	def new
-		@blog = Blog.new
+  def show
+    @blog = Blog.find params[:id]
   end
 
-	def create
-		@blog = Blog.new blog_params
-		if @blog.save
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.new blog_params
+    if @blog.save
       flash[:success] = "Create success"
       redirect_to blog_path(@blog, create_success: true)
     else
       flash.now[:error] = "Create fail"
       format.html{render :new}
     end
-	end
+  end
 
-	private
-	def blog_params
-		params.require(:blog).permit(:title, :content)
-	end
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
 end
